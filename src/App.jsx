3dea43e31dd7c77d792a28df09fde9b28dc5b50c
hyeyeong <kkotburiwillbloom@gmail.com>
2023-06-15 10:18:47 +0900

    isDone rendering

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,18 @@ import Button from "./components/Button";
 
 const App = () => {
   const [todolist, setTodoList] = useState([
-    { id: 1, title: "algorithm 학습", content: "최빈값 구하기" },
-    { id: 2, title: "TIL 작성", content: "2023년 6월 14일" },
+    { id: 1, title: "algorithm 학습", content: "최빈값 구하기", isDone: false },
+    { id: 2, title: "TIL 작성", content: "2023년 6월 14일", isDone: true },
   ]);
 
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
 
-  const nameChangeHander = (event) => {
+  const titleChangeHander = (event) => {
     setTitle(event.target.value);
   };
 
-  const ageChangeHander = (event) => {
+  const contentChangeHander = (event) => {
     setContent(event.target.value);
   };
 
@@ -27,6 +27,7 @@ const App = () => {
       id: todolist.length + 1,
       title,
       content,
+      done: false,
     };
 
     // 2. 배열에 추가
@@ -42,12 +43,40 @@ const App = () => {
     setTodoList(newTodolist);
   };
 
+  // 완료 button
+  const clickDoneButtonHandler = (id) => {
+    setTodoList(
+      todolist.map((todo) =>
+        todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
+      )
+    );
+  };
+
+  // 완료 button 'done'
+  // const clickDoneButtonHandler = (done) => {
+  //   const isDone = done ? false : true;
+  //   console.log(isDone);
+  //   setDone("취소");
+  // };
+
+  // 완료 button 'count'
+  // const [count, setCount] = useState(0);
+  // const [done, setDone] = useState("완료");
+
+  // const clickDoneButtonHandler = (count) => {
+  //   const plusCount = count + 1;
+  //   setCount(plusCount);
+  //   console.log(plusCount);
+  //   const isDone = count % 2 === 0 ? "취소" : "완료";
+  //   setDone(isDone);
+  // };
+
   return (
     <div className="app">
       <div className="input">
         <div>
-          제목:&nbsp; <input value={title} onChange={nameChangeHander} />
-          내용:&nbsp; <input value={content} onChange={ageChangeHander} />
+          제목:&nbsp; <input value={title} onChange={titleChangeHander} />
+          내용:&nbsp; <input value={content} onChange={contentChangeHander} />
         </div>
         <Button addFunction={clickAddButtonHandler}>추가</Button>
       </div>
@@ -58,6 +87,7 @@ const App = () => {
               key={todo.id}
               todo={todo}
               removeFunction={clickRemoveButtonHandler}
+              doneFunction={clickDoneButtonHandler}
             />
           );
         })}
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,4 +1,4 @@
-const Todo = ({ todo, removeFunction }) => {
+const Todo = ({ todo, todo: { isDone }, removeFunction, doneFunction }) => {
   return (
     <div className="todo">
       <div className="todo-title">{todo.title}</div>
@@ -10,7 +10,12 @@ const Todo = ({ todo, removeFunction }) => {
         >
           삭제
         </button>
-        <button className="todo-button-done">완료</button>
+        <button
+          className="todo-button-done"
+          onClick={() => doneFunction(todo.id)}
+        >
+          {isDone ? "취소" : "완료"}
+        </button>
       </div>
     </div>
   );
